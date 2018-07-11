5db2de017222e0a6e2f54aa6eec52da6bb8cc013
Jonas Antonio Gome Vicente <jayv.jonas@gmail.com>
2018-07-10 23:07:40 -0300

    Classe Usuario criada

--- dao/index.php
+++ dao/index.php
@@ -1,11 +1,16 @@
 <?php 
 
 	require_once("config.php");
-
+	/*
 	$sql = new Sql();
 
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 	echo json_encode($usuarios);
-	
+	*/
+
+	$usuario = new Usuario();
+	$usuario->loadById(3);
+
+	echo $usuario;
  ?>
