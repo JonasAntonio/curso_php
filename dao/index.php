315a9e3e4e38f6f5b43553ffba5a0a0b9f287ad4
Jonas Antonio Gome Vicente <jayv.jonas@gmail.com>
2018-07-11 19:31:47 -0300

    Mais funções na classe Usuario

--- dao/index.php
+++ dao/index.php
@@ -9,8 +9,24 @@
 	echo json_encode($usuarios);
 	*/
 
+	/* Carrega um usuario
 	$usuario = new Usuario();
 	$usuario->loadById(3);
 
 	echo $usuario;
+	*/
+	/*Carrega uma lista de usuarios
+	$listaUsuarios = Usuario::getList();
+
+	echo json_encode($listaUsuarios);
+	*/
+	/*Carrega uma lista de usuarios buscando pelo login
+	$search = Usuario::search("r");
+	echo json_encode($search);
+	*/
+	//Carrega um usuario pelo login e senha
+	$usuario = new Usuario();
+	$usuario->login("user", "123456");
+	echo $usuario;
+
  ?>
