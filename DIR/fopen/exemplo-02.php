e0214b077eb3d891516ae1ad11dc428de5b878a0
Jonas Antonio Gome Vicente <jayv.jonas@gmail.com>
2018-07-16 10:21:10 -0300

    fopen(), fwrite() e fclose()

---
+++ DIR/fopen/exemplo-02.php
@@ -0,0 +1,29 @@
+<?php 
+
+	require_once("config.php");
+
+	$sql = new Sql();
+
+	$usuarios = $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+	$headers = array();
+
+	foreach ($usuarios[0] as $key => $value) {
+		array_push($headers, ucfirst($key));
+	}
+
+	$file = fopen("usuarios.csv", "w+");
+	fwrite($file, implode(",", $headers)."\r\n");
+	foreach ($usuarios as $row) {//linhas
+		$data = array();
+		foreach ($row as $key => $value) {//campos
+			array_push($data, $value);
+		}	
+		fwrite($file, implode(",", $data)."\r\n");
+	}
+
+	fclose($file);
+
+	echo "Arquivo criado com sucesso!";
+
+ ?>
